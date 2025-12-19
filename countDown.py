c8c51a5dac4057da33f3d44695414b0d0e256d3a
aarokan <aarokan@gmail.com>
2025-12-19 23:00:56 +0300

    extract even numbers

--- countDown.py
+++ countDown.py
@@ -4,15 +4,25 @@ def countdown(num: int) -> List[int]:
     list_result = create_list(num)
     print(list_result)
 
+    filter_even_numbers(list_result)
+    list_result.reverse()
+    print(list_result)
+    return list_result
 
 def create_list(num:int) -> List[int]:
     new_list = []
-    if num < 3:
+    if num <= 3:
         return [0]
     else:
-        for i in range(num, 0, -3):
+        # start from num-3 untill 0 with step -3s
+        for i in range(num-3, 0, -3):
             new_list.append(i)
         return new_list
 
+def filter_even_numbers(created_list: list) -> None:
+    for i in created_list:
+        if i % 2 != 0:
+            created_list.remove(i)
+    
 
 countdown(10)
