b0e8936d5835555e9e583fa5b79daaaaa6e010a9
aarokan <aarokan@gmail.com>
2025-12-24 19:02:47 +0300

    alternating substring: define is odd function

--- alternatingSubstring.py
+++ alternatingSubstring.py
@@ -24,11 +24,15 @@ def extract_alternating_substrings(alternating_list: list, digits: str) -> None:
     # avoid not having a pair of two elements at the end of list
     while i <= digits_length - 2:
         print(f" i: {i},  1st: {digits[i]}, 2nd: {digits[i + 1]}")
-        if not is_started and are_alternated(int(digits[i]), int(digits[i + 1])):
+        # check if alternated substring is going to start
+        if not is_started and are_alternated(digits[i], digits[i + 1]):
             start_index = i
             is_started = True
             is_odd_started = True if int(digits[i]) % 2 != 0 else False
             print("start_index: ", start_index)
+        # check if the substring not initiated with odd num or the current digit is not odd  
+        elif not is_odd_started or not is_odd(digits[i]):
+            print("is_odd: ", is_odd(digits[i]))
         
         # avoid not having a pair of two elements at the end of list
         if digits_length - i == 3:
@@ -40,19 +44,27 @@ def extract_alternating_substrings(alternating_list: list, digits: str) -> None:
 # define find_longest function to find the longest substring from a list of substrings
 
 
-# define function to check if the number is even
-def are_alternated(first_num: int, second_num: int) -> bool:
+# define function to check if two numbers are alternated
+def are_alternated(first_num: str, second_num: str) -> bool:
     """ checks two numbers if they are alternated (odd and even) """
-    if first_num % 2 == 0 and second_num % 2 != 0:
+    int_first_num: int = int(first_num)
+    int_second_num: int = int(second_num)
+
+    if int_first_num % 2 == 0 and int_second_num % 2 != 0:
         return True
-    elif first_num % 2 != 0 and second_num % 2 == 0:
+    elif int_first_num % 2 != 0 and int_second_num % 2 == 0:
         return True
     else:
         return False
 
 
+# define function to check if the number is odd
+def is_odd(num: str) -> bool:
+    int_num: int = int(num)
+    return True if int_num % 2 != 0 else False
+
 # digits = "2105787220351146"
-digits = "21057872203511461"
+digits = "12057872203511461"
 # digits = "213"
 
 longest_alternating_substring(digits)
